271f2cc1d4f6fe1ef4d44132785818b04efef244
Brian Rumschlag <brian@rumschlag.com>
2017-04-04 10:28:29 -0400

    Added KafkaTest projects

---
+++ KafkaMessages/PingMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaMessages
+{
+    public class PingRequest :SlimMessageBus.IRequestMessage<PingResponse>
+    {
+        public DateTimeOffset Timestamp { get; set; }
+        public string Key { get; set; }
+    }
+    public class PingResponse
+    {
+        public DateTimeOffset Timestamp { get; set; }
+        public string Key { get; set; }
+    }
+}
